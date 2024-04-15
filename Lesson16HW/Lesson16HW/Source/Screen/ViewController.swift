c38a066585731b7dab46310424fb822acbfe3aeb
LynnVlasenko <lynn.vlasenko@gmail.com>
2024-04-15 12:55:42 +0200

    Added project. Organized folders.

---
+++ Lesson16HW/Lesson16HW/Source/Screen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson16HW
+//
+//  Created by Алина Власенко on 15.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+    }
+
+
+}
+
